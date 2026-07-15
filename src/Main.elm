c5010f910d3af4121f5879b2d901b8592989e562
Benjamin Lautenbach <93003415+tim-lautenbach-uni@users.noreply.github.com>
2026-07-15 18:28:10 +0200

    Spiele und Ergebnisse Übersicht + Gruppierung

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,17 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, h1, h2, table, tbody, td, text, th, thead, tr)
-import Html.Attributes exposing (class)
+import Games exposing (Game, games)
+import Html exposing (Html, button, div, h1, h2, h3, h4, p, span, table, tbody, td, text, th, thead, tr)
+import Html.Attributes exposing (class, disabled)
 import Html.Events exposing (onClick)
 
 
-type ViewMode
-    = Groups
-    | List
+type Page
+    = Home
+    | Countries
+    | Groups
+    | Matches
 
 
 type SortMode
@@ -18,24 +21,42 @@ type SortMode
     | FifaRankingDescending
 
 
+type MatchGrouping
+    = ByDays
+    | ByGroups
+    | ByMatchdays
+
+
 type alias Model =
-    { viewMode : ViewMode
+    { page : Page
     , sortMode : SortMode
     , sortMenuOpen : Bool
+    , matchGrouping : MatchGrouping
+    , matchGroupingMenuOpen : Bool
     }
 
 
 type Msg
-    = ShowGroups
-    | ShowList
+    = ShowHome
+    | ShowCountries
+    | ShowGroups
+    | ShowMatches
     | ToggleSortMenu
     | SetSort SortMode
+    | ToggleMatchGroupingMenu
+    | SetMatchGrouping MatchGrouping
 
 
 main : Program () Model Msg
 main =
     Browser.sandbox
-        { init = { viewMode = Groups, sortMode = AlphabeticalAscending, sortMenuOpen = False }
+        { init =
+            { page = Home
+            , sortMode = AlphabeticalAscending
+            , sortMenuOpen = False
+            , matchGrouping = ByDays
+            , matchGroupingMenuOpen = False
+            }
         , update = update
         , view = view
         }
@@ -44,11 +65,17 @@ main =
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        ShowGroups ->
-            { model | viewMode = Groups, sortMenuOpen = False }
+        ShowHome ->
+            { model | page = Home, sortMenuOpen = False, matchGroupingMenuOpen = False }
 
-        ShowList ->
-            { model | viewMode = List, sortMenuOpen = False }
+        ShowCountries ->
+            { model | page = Countries, sortMenuOpen = False, matchGroupingMenuOpen = False }
+
+        ShowGroups ->
+            { model | page = Groups, sortMenuOpen = False, matchGroupingMenuOpen = False }
+
+        ShowMatches ->
+            { model | page = Matches, sortMenuOpen = False, matchGroupingMenuOpen = False }
 
         ToggleSortMenu ->
             { model | sortMenuOpen = not model.sortMenuOpen }
@@ -56,29 +83,57 @@ update msg model =
         SetSort sortMode ->
             { model | sortMode = sortMode, sortMenuOpen = False }
 
+        ToggleMatchGroupingMenu ->
+            { model | matchGroupingMenuOpen = not model.matchGroupingMenuOpen, sortMenuOpen = False }
+
+        SetMatchGrouping grouping ->
+            { model | matchGrouping = grouping, matchGroupingMenuOpen = False }
+
 
 view : Model -> Html Msg
 view model =
     div [ class "page" ]
-        [ h1 [] [ text "World Cup Guesser" ]
-        , div [ class "button-row" ]
-            [ button [ onClick ShowGroups ] [ text "Gruppen" ]
-            , button [ onClick ShowList ] [ text "Liste" ]
-            ]
-        , case model.viewMode of
-            Groups ->
-                div [] (List.map viewGroup groupNames)
+        [ case model.page of
+            Home ->
+                viewHome
 
-            List ->
-                viewList model
+            Countries ->
+                viewSubpage "Länderliste" (viewList model)
+
+            Groups ->
+                viewSubpage "Gruppen" (div [] (List.map viewGroup groupNames))
+
+            Matches ->
+                viewSubpage "Spiele und Ergebnisse" (viewMatches model)
+        ]
+
+
+viewHome : Html Msg
+viewHome =
+    div [ class "home" ]
+        [ h1 [] [ text "World Cup Guesser" ]
+        , div [ class "home-menu" ]
+            [ button [ onClick ShowCountries ] [ text "Länderliste" ]
+            , button [ onClick ShowGroups ] [ text "Gruppenansicht" ]
+            , button [ onClick ShowMatches ] [ text "Spiele und Ergebnisse" ]
+            , button [ disabled True ] [ text "Quiz" ]
+            ]
+        ]
+
+
+viewSubpage : String -> Html Msg -> Html Msg
+viewSubpage title content =
+    div []
+        [ button [ class "back-button", onClick ShowHome ] [ text "Zurück zur Startseite" ]
+        , h1 [] [ text title ]
+        , content
         ]
 
 
 viewList : Model -> Html Msg
 viewList model =
     div []
-        [ h2 [] [ text "Alle Länder" ]
-        , div [ class "sort-dropdown" ]
+        [ div [ class "sort-dropdown" ]
             [ button [ class "sort-button", onClick ToggleSortMenu ]
                 [ text
                     (if model.sortMenuOpen then
@@ -112,6 +167,261 @@ viewList model =
                 ]
             ]
         ]
+
+
+viewMatches : Model -> Html Msg
+viewMatches model =
+    div []
+        [ p [] [ text "Stand: 15.07.2026 · Uhrzeiten in deutscher Sommerzeit" ]
+        , div [ class "sort-dropdown" ]
+            [ button [ class "sort-button", onClick ToggleMatchGroupingMenu ]
+                [ text
+                    (if model.matchGroupingMenuOpen then
+                        "Gruppieren nach ▲"
+
+                     else
+                        "Gruppieren nach ▼"
+                    )
+                ]
+            , if model.matchGroupingMenuOpen then
+                div [ class "sort-menu" ]
+                    [ button [ onClick (SetMatchGrouping ByGroups) ] [ text "Gruppen" ]
+                    , button [ onClick (SetMatchGrouping ByDays) ] [ text "Tage" ]
+                    , button [ onClick (SetMatchGrouping ByMatchdays) ] [ text "Spieltage" ]
+                    ]
+
+              else
+                text ""
+            ]
+        , case model.matchGrouping of
+            ByDays ->
+                viewDays
+
+            ByGroups ->
+                viewGamesByGroup
+
+            ByMatchdays ->
+                viewMatchdays
+        ]
+
+
+viewDays : Html Msg
+viewDays =
+    div []
+        [ viewDaySection "Gruppenphase" groupStageGames
+        , viewDaySection "Ausscheidungsspiele" knockoutGames
+        ]
+
+
+viewDaySection : String -> List Game -> Html Msg
+viewDaySection title sectionGames =
+    div [ class "results-section" ]
+        (h2 [ class "results-section-title" ] [ text title ]
+            :: (sectionGames
+                    |> List.map .date
+                    |> unique
+                    |> List.map (viewDay sectionGames)
+               )
+        )
+
+
+viewDay : List Game -> String -> Html Msg
+viewDay sectionGames date =
+    div [ class "match-block" ]
+        [ h3 [] [ text date ]
+        , sectionGames
+            |> List.filter (\match -> match.date == date)
+            |> viewGameTable False True
+        ]
+
+
+viewGamesByGroup : Html Msg
+viewGamesByGroup =
+    div []
+        [ div [ class "results-section" ]
+            (h2 [ class "results-section-title" ] [ text "Gruppenphase" ]
+                :: List.map viewMatchGroup groupNames
+            )
+        , div [ class "results-section" ]
+            (h2 [ class "results-section-title" ] [ text "Ausscheidungsspiele" ]
+                :: List.map viewKnockoutRound knockoutStages
+            )
+        ]
+
+
+viewMatchdays : Html Msg
+viewMatchdays =
+    div []
+        [ div [ class "results-section" ]
+            (h2 [ class "results-section-title" ] [ text "Gruppenphase" ]
+                :: List.map viewMatchday [ 1, 2, 3 ]
+            )
+        , div [ class "results-section" ]
+            (h2 [ class "results-section-title" ] [ text "Ausscheidungsspiele" ]
+                :: List.map viewKnockoutRound knockoutStages
+            )
+        ]
+
+
+viewMatchday : Int -> Html Msg
+viewMatchday matchday =
+    let
+        matchdayGames =
+            gamesForMatchday matchday
+    in
+    div [ class "matchday-block" ]
+        (h3 [ class "matchday-title" ] [ text ("Spieltag " ++ String.fromInt matchday) ]
+            :: (matchdayGames
+                    |> List.map .date
+                    |> unique
+                    |> List.map (viewMatchdayDate matchdayGames)
+               )
+        )
+
+
+viewMatchdayDate : List Game -> String -> Html Msg
+viewMatchdayDate matchdayGames date =
+    div [ class "match-block" ]
+        [ h4 [] [ text date ]
+        , matchdayGames
+            |> List.filter (\game -> game.date == date)
+            |> viewGameTable False True
+        ]
+
+
+gamesForMatchday : Int -> List Game
+gamesForMatchday matchday =
+    List.filter (belongsToMatchday matchday) groupStageGames
+
+
+belongsToMatchday : Int -> Game -> Bool
+belongsToMatchday matchday currentGame =
+    groupStageGames
+        |> List.filter (\game -> game.stage == currentGame.stage)
+        |> List.drop ((matchday - 1) * 2)
+        |> List.take 2
+        |> List.any (\game -> game.number == currentGame.number)
+
+
+viewMatchGroup : String -> Html Msg
+viewMatchGroup groupName =
+    viewStageBlock
+        ("Gruppe " ++ groupName)
+        (List.filter (\match -> match.stage == "Gruppe " ++ groupName) groupStageGames)
+
+
+viewKnockoutRound : String -> Html Msg
+viewKnockoutRound stage =
+    viewStageBlock stage (List.filter (\match -> match.stage == stage) knockoutGames)
+
+
+viewStageBlock : String -> List Game -> Html Msg
+viewStageBlock title stageGames =
+    div [ class "match-block" ]
+        [ h3 [] [ text title ]
+        , viewGameTable True False stageGames
+        ]
+
+
+viewGameTable : Bool -> Bool -> List Game -> Html Msg
+viewGameTable showDate showStage currentGames =
+    div [ class "table-scroll" ]
+        [ table [ class "data-table matches-table" ]
+            [ thead []
+                [ tr []
+                    ((if showDate then
+                        [ th [ class "date-cell" ] [ text "Datum" ] ]
+
+                      else
+                        []
+                     )
+                        ++ [ th [ class "time-cell" ] [ text "Uhrzeit" ]
+                           ]
+                        ++ (if showStage then
+                                [ th [ class "stage-cell" ] [ text (stageColumnTitle currentGames) ] ]
+
+                            else
+                                []
+                           )
+                        ++ [ th [ class "game-cell" ] [ text "Spiel" ] ]
+                    )
+                ]
+            , tbody [] (List.map (viewGameRow showDate showStage) currentGames)
+            ]
+        ]
+
+
+viewGameRow : Bool -> Bool -> Game -> Html Msg
+viewGameRow showDate showStage match =
+    tr []
+        ((if showDate then
+            [ td [ class "date-cell" ] [ text match.date ] ]
+
+          else
+            []
+         )
+            ++ [ td [ class "time-cell" ] [ text match.time ] ]
+            ++ (if showStage then
+                    [ td [ class "stage-cell" ] [ text match.stage ] ]
+
+                else
+                    []
+               )
+            ++ [ td [ class "game-cell" ] [ viewFixture match ] ]
+        )
+
+
+stageColumnTitle : List Game -> String
+stageColumnTitle currentGames =
+    if List.any (\game -> not (String.startsWith "Gruppe " game.stage)) currentGames then
+        "K.O.-Runde"
+
+    else
+        "Gruppe"
+
+
+viewFixture : Game -> Html Msg
+viewFixture match =
+    div [ class "fixture" ]
+        [ span [ class "fixture-home" ] [ text match.home ]
+        , span [ class "fixture-score" ] [ text match.score ]
+        , span [ class "fixture-away" ] [ text match.away ]
+        ]
+
+
+groupStageGames : List Game
+groupStageGames =
+    List.filter (\match -> String.startsWith "Gruppe " match.stage) games
+
+
+knockoutGames : List Game
+knockoutGames =
+    List.filter (\match -> not (String.startsWith "Gruppe " match.stage)) games
+
+
+knockoutStages : List String
+knockoutStages =
+    [ "Sechzehntelfinale"
+    , "Achtelfinale"
+    , "Viertelfinale"
+    , "Halbfinale"
+    , "Spiel um Platz 3"
+    , "Finale"
+    ]
+
+
+unique : List comparable -> List comparable
+unique values =
+    List.foldl
+        (\value result ->
+            if List.member value result then
+                result
+
+            else
+                result ++ [ value ]
+        )
+        []
+        values
 
 
 sortTeams : SortMode -> List Team -> List Team
